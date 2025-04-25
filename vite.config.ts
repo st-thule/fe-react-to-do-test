c322634eaeb835df125ffcf7b323d20ecc404fc8
st-thule <thu.le@supremetech.vn>
2025-04-25 09:15:31 +0700

    feat: build structure of project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: './src',
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      /**
+       * After writing entry here, you will not need to add script tags in `index.html`, the original tags need to be deleted
+       * @default /app/App.tsx
+       */
+      entry: '/app/App.tsx',
+      /**
+       * If you want to store `index.html` in the specified folder, you can modify it, otherwise no configuration is required
+       * @default index.html
+       */
+      template: 'index.html',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@config': path.resolve(__dirname, './src/config'),
+      '@stylesheet': path.resolve(__dirname, './src/app/stylesheet'),
+      '@shared': path.resolve(__dirname, './src/app/shared'),
+      '@core': path.resolve(__dirname, './src/app/core'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
